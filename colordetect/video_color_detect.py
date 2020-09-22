fc903b4864d192e23b6088c157cc2d64553d45d7
MarvinKweyu <mkweyu1@gmail.com>
2020-09-22 23:33:45 +0300

    Get video frames and their color descriptions and get top from the whole

--- colordetect/video_color_detect.py
+++ colordetect/video_color_detect.py
@@ -1,4 +1,5 @@
 import os
+import itertools
 import cv2
 from .color_detect import ColorDetect
 
@@ -10,6 +11,7 @@ class VideoColor(ColorDetect):
     def __init__(self, video):
         super().__init__(video)
         self.video_file = cv2.VideoCapture(video)
+        self.color_description = {}
 
     def get_video_frames(self):
         """
@@ -21,17 +23,25 @@ class VideoColor(ColorDetect):
             # frame is the image
             success, frame = self.video_file.read()
             if success:
-                print(f'Frame is of type: {type(frame)}')
                 # save frame as JPEG file
-                # cv2.imwrite(os.path.join("Random/frame{:d}.jpg".format(count)), frame)
-                # image = cv2.imencode(img=frame)
                 image_object = ColorDetect(frame)
-                image_object.get_color_count()
-                image_object.write_color_count()
-                storage_path = os.path.join("Random/frame{:d}.jpg".format(count))
-                image_name = "frame" + str(count) + ".jpg"
-                image_object.save_image("Random", image_name)()
+                colors = image_object.get_color_count()
+                # merge dictionaries as they are created
+                self.color_description = {**self.color_description, **colors}
+                # image_object.write_color_count()
+                # storage_path = os.path.join("Random/frame{:d}.jpg".format(count))
+                # image_name = "frame" + str(count) + ".jpg"
+                # image_object.save_image("Random", image_name)
                 count += 1
 
         self.video_file.release()
         cv2.destroyAllWindows()
+        return self.color_description
+
+    def sorted_colors(self, color_count: int = 5):
+        """
+        Get number of colors wanted from video
+        :return:
+        """
+        sorted_colors = {k: v for k, v in sorted(self.color_description.items(), key=lambda item: item[1])}
+        return dict(list(sorted_colors.items())[0: color_count])
