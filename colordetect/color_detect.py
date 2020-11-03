140313b883f947bd5ceb6dc76dcd9cf042c99d47
Marvin Kweyu <mkweyu1@gmail.com>
2020-11-03 08:44:26 +0300

    Feature/col share (#12)
    * Split module independent processes and update docs and tests
    
    - update tracked files to sync with gitignore
    
    * Perform check on availability of color description in write
    
    * docs: update documentation with col_share
    
    * docs: show correct date of release 1.0.0 in change log

--- colordetect/color_detect.py
+++ colordetect/color_detect.py
@@ -68,7 +68,9 @@ class ColorDetect:
         """
 
         if type(color_count) != int:
-            raise TypeError(f"color_count has to be an integer. Provided {type(color_count)} ")
+            raise TypeError(
+                f"color_count has to be an integer. Provided {type(color_count)} "
+            )
 
         # convert image from BGR to RGB for better accuracy
         rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
@@ -115,8 +117,9 @@ class ColorDetect:
         hist /= hist.sum()
 
         # iterate through each cluster's color and percentage
-        colors = sorted([((percent * 100), color)
-                         for (percent, color) in zip(hist, centroids)])
+        colors = sorted(
+            [((percent * 100), color) for (percent, color) in zip(hist, centroids)]
+        )
 
         for (percent, color) in colors:
             color.astype("uint8").tolist()
@@ -153,7 +156,6 @@ class ColorDetect:
             Thickness of the text
         line_type: int = 1,
         """
-
         if not self.color_description:
             raise AttributeError(
                 "No color description found on this object. Perform get_color_count() first."
@@ -214,7 +216,9 @@ class ColorDetect:
         :return:
         """
         if type(text) != str:
-            raise TypeError(f"text should be a string.Provided {text} of type {type(text)}")
+            raise TypeError(
+                f"text should be a string.Provided {text} of type {type(text)}"
+            )
 
         if text == "":
             raise IOError("text should not be empty")
