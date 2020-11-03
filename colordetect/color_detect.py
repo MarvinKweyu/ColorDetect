8fefa5408a21ef2b4fb0f1f4f8c74682335ea8e9
Marvin Kweyu <mkweyu1@gmail.com>
2020-11-03 08:44:26 +0300

    Feature/col share (#12)
    * Split module independent processes and update docs and tests
    
    - update tracked files to sync with gitignore
    
    * Perform check on availability of color description in write
    
    * docs: update documentation with col_share
    
    * docs: show correct date of release 1.0.0 in change log

--- colordetect/color_detect.py
+++ colordetect/color_detect.py
@@ -68,7 +68,9 @@ class ColorDetect:
         """
 
         if type(color_count) != int:
-            raise TypeError(f"color_count has to be an integer. Provided {type(color_count)} ")
+            raise TypeError(
+                f"color_count has to be an integer. Provided {type(color_count)} "
+            )
 
         # convert image from BGR to RGB for better accuracy
         rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
@@ -115,8 +117,9 @@ class ColorDetect:
         hist /= hist.sum()
 
         # iterate through each cluster's color and percentage
-        colors = sorted([((percent * 100), color)
-                         for (percent, color) in zip(hist, centroids)])
+        colors = sorted(
+            [((percent * 100), color) for (percent, color) in zip(hist, centroids)]
+        )
 
         for (percent, color) in colors:
             color.astype("uint8").tolist()
@@ -128,10 +131,10 @@ class ColorDetect:
         -----------------
         Write the number of colors found to the image
         """
-
         if not self.color_description:
-            raise AttributeError(f"No color description found on this object. Perform get_color_count() first.")
-
+            raise AttributeError(
+                f"No color description found on this object. Perform get_color_count() first."
+            )
         line_spacing = 0
         for k, v in self.color_description.items():
             color_values = str(v) + "% :" + k
@@ -154,7 +157,9 @@ class ColorDetect:
         :return:
         """
         if type(text) != str:
-            raise TypeError(f"text should be a string.Provided {text} of type {type(text)}")
+            raise TypeError(
+                f"text should be a string.Provided {text} of type {type(text)}"
+            )
 
         if text == "":
             raise IOError("text should not be empty")
@@ -167,11 +172,15 @@ class ColorDetect:
         fontColor = (0, 0, 0)
         lineType = 1
 
-        cv2.putText(self.image, text, bottomLeftCornerOfText,
-                    font,
-                    fontScale,
-                    fontColor,
-                    lineType)
+        cv2.putText(
+            self.image,
+            text,
+            bottomLeftCornerOfText,
+            font,
+            fontScale,
+            fontColor,
+            lineType,
+        )
 
     def save_image(self, location=".", file_name: str = "out.jpg"):
         """
