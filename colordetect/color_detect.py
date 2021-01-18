dc1f567c253a66ed142dfc4769ecd4097d302c5a
Clifford Onyonka <jsjamessakho@gmail.com>
2021-01-18 18:31:01 +0300

    Add human-readable colours (#18)
    * Add human-readable colours to output
    
    * Update test to work with human readable colors
    
    * Add webcolors dependency
    
    Co-authored-by: MarvinKweyu <mkweyu1@gmail.com>
    Co-authored-by: Clifford <clifford@swiftlabs.ai>

--- colordetect/color_detect.py
+++ colordetect/color_detect.py
@@ -47,7 +47,9 @@ class ColorDetect:
 
         self.color_description = {}
 
-    def get_color_count(self, color_count: int = 5, color_format: str = "human_readable") -> dict:
+    def get_color_count(
+        self, color_count: int = 5, color_format: str = "human_readable"
+    ) -> dict:
         """
         .. _get_color_count:
         get_color_count
@@ -205,7 +207,6 @@ class ColorDetect:
         font_scale: float = 1.0,
         font_thickness: float = 1.0,
         line_type: int = 1,
-        line_spacing: int = 0
     ):
         """
         .. _write_text:
@@ -232,7 +233,6 @@ class ColorDetect:
         font_thickness:
             Thickness of the text
         line_type: int = 1,
-            Space betweeen the lines
         :return:
         """
         if type(text) != str:
@@ -252,7 +252,6 @@ class ColorDetect:
             font_color,
             font_thickness,
             line_type,
-            line_spacing,
         )
 
     def save_image(self, location=".", file_name: str = "out.jpg"):
