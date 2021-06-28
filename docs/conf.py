af343d661b281e68448bfc2b48f24738eabfb155
MarvinKweyu <mkweyu1@gmail.com>
2021-06-28 09:04:53 +0300

    docs: update package version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Marvin Kweyu"
 author = "Marvin Kweyu"
 
 # The full version, including alpha/beta/rc tags
-release = "1.4.3"
+release = "1.4.5"
 
 # -- General configuration ---------------------------------------------------
 
