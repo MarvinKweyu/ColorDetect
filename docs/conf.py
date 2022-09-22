959abc31be92cf70069abae67de7df964ce30dbf
Marvin Kweyu <mkweyu1@gmail.com>
2022-09-22 11:03:01 +0300

    docs: update video_color docs ad add internal links on doc page

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.viewcode",  # package rst sources with docs
     "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
